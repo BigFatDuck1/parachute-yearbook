48c5828f81e6ade1102653b04b867f2ddc521c15
BigFatDuck1 <clementho999@gmail.com>
2023-11-12 15:57:21 +0800

    Added parallax scrolling

--- scripts.js
+++ scripts.js
@@ -12,3 +12,20 @@ const closeModal = () => {
     })
 }
 closeModal();
+
+//Parallax
+window.addEventListener("scroll", function setParallax() {
+    const translucent_clouds = document.querySelectorAll(".translucent");
+    const opaque_clouds = document.querySelectorAll(".opaque");
+
+    translucent_clouds.forEach((element) => {
+        let speed = -1.5;
+        element.style.transform = `translateY(${window.scrollY * speed}px)`;
+    })
+
+    opaque_clouds.forEach((element) => {
+        let speed = -1;
+        element.style.transform = `translateY(${window.scrollY * speed}px)`;
+    })
+
+})
