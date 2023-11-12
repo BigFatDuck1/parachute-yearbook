c95719187973a969ea6f9253e942c5484e0b359e
BigFatDuck1 <clementho999@gmail.com>
2023-11-12 16:16:19 +0800

    Added balloon button (with just placeholder text, no image right now)

--- scripts.js
+++ scripts.js
@@ -1,20 +1,10 @@
 
-// Modal functions
-document.addEventListener("keydown", function (event) {
-    if (event.key == "a") {
-        document.querySelector("dialog").showModal();
-    }
-});
-
-const closeModal = () => {
-    document.querySelector("#close_button").addEventListener("click", function () {
-        document.querySelector("dialog").close();
-    })
-}
-closeModal();
-
 //Parallax
 window.addEventListener("scroll", function setParallax() {
+
+    //Add zoomout animation to year
+    document.querySelector(".year").classList.add("year_zoomout");
+
     const translucent_clouds = document.querySelectorAll(".translucent");
     const opaque_clouds = document.querySelectorAll(".opaque");
 
@@ -29,3 +19,27 @@ window.addEventListener("scroll", function setParallax() {
     })
 
 })
+
+//Balloon and Modal
+    // Modal functions
+// document.addEventListener("keydown", function (event) {
+//     if (event.key == "a") {
+//         document.querySelector("dialog").showModal();
+//     }
+// });
+
+const all_balloons = document.querySelectorAll(".balloon");
+
+    //Open modal when the balloon is clicked
+all_balloons.forEach((element) => {
+    element.addEventListener("click", () => {
+        document.querySelector("dialog").showModal();
+    });
+})
+
+const closeModal = () => {
+    document.querySelector("#close_button").addEventListener("click", function () {
+        document.querySelector("dialog").close();
+    })
+}
+closeModal();
